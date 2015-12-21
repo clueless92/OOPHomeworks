bb7d6836e01ac1fff80306b7308790540ed617f7
clueless92 <tilchev92@gmail.com>
2015-12-21 19:45:46 +0200

    Upload

--- OOPExamPreparation/src/OOPSampleExam14Dec2015Empires/core/factories/BuildingFactoryImpl.java
+++ OOPExamPreparation/src/OOPSampleExam14Dec2015Empires/core/factories/BuildingFactoryImpl.java
@@ -4,65 +4,62 @@ import OOPSampleExam14Dec2015Empires.contracts.Building;
 import OOPSampleExam14Dec2015Empires.contracts.BuildingFactory;
 import OOPSampleExam14Dec2015Empires.contracts.ResourceFactory;
 import OOPSampleExam14Dec2015Empires.contracts.UnitFactory;
+import OOPSampleExam14Dec2015Empires.core.MyReflection;
 import OOPSampleExam14Dec2015Empires.models.buildings.Archery;
 import OOPSampleExam14Dec2015Empires.models.buildings.Barracks;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.StandardLocation;
+import javax.tools.ToolProvider;
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class BuildingFactoryImpl implements BuildingFactory {
 
     @Override
     public Building createBuilding(String buildingType, UnitFactory unitFactory, ResourceFactory resourceFactory) {
 
-        switch (buildingType) {
-            case "barracks":
-                return new Barracks(unitFactory, resourceFactory);
-            case "archery":
-                return new Archery(unitFactory, resourceFactory);
-            default:
-                throw new UnsupportedOperationException("Unknown building type");
+        final String pack = "OOPSampleExam14Dec2015Empires.models.buildings";
+        Collection<Class> classList = new ArrayList<>();
+        Constructor[] ctors = null;
+        Building building = null;
+        try {
+            classList = MyReflection.getClasses(pack);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
-
-    private static List<Class> getClasses(ClassLoader cl,String pack) throws Exception{
-
-        URL upackage = cl.getResource(pack);
-
-        BufferedReader br = null;
-        Object upackageContent = null;
-        if (upackage != null) {
-            upackageContent = upackage.getContent();
-        }
-        InputStreamReader isr = null;
-        if (upackageContent != null) {
-            isr = new InputStreamReader((InputStream) upackageContent);
-        }
-        if (isr != null) {
-            br = new BufferedReader(isr);
-        }
-        String dottedPackage = pack.replaceAll("[/]", ".");
-
-        List<Class> classes = new ArrayList<>();
-        String line = null;
-        if (br != null) {
-            line = br.readLine();
-        }
-        while (line != null) {
-            if(line.endsWith(".class")) {
-                int lastIndex = line.lastIndexOf('.');
-                if (lastIndex >= 0) {
-                    String className = dottedPackage + "." + line.substring(0, lastIndex);
-                    Class c = Class.forName(className);
-                    classes.add(c);
+        for(Class c : classList) {
+            if ((c.getSimpleName().toLowerCase()).equals(buildingType)) {
+                ctors = c.getConstructors();
+                if (ctors != null) {
+                    try {
+                        building = (Building) ctors[0].newInstance(unitFactory, resourceFactory);
+                    } catch (InstantiationException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-            line = br.readLine();
         }
-        return classes;
+        return building;
+//        switch (buildingType) {
+//            case "barracks":
+//                return new Barracks(unitFactory, resourceFactory);
+//            case "archery":
+//                return new Archery(unitFactory, resourceFactory);
+//            default:
+//                throw new UnsupportedOperationException("Unknown building type");
+//        }
     }
 }
--- OOPExamPreparation/src/OOPSampleExam14Dec2015Empires/core/factories/UnitFactoryImpl.java
+++ OOPExamPreparation/src/OOPSampleExam14Dec2015Empires/core/factories/UnitFactoryImpl.java
@@ -2,90 +2,59 @@ package OOPSampleExam14Dec2015Empires.core.factories;
 
 import OOPSampleExam14Dec2015Empires.contracts.Unit;
 import OOPSampleExam14Dec2015Empires.contracts.UnitFactory;
-import OOPSampleExam14Dec2015Empires.models.units.Archer;
-import OOPSampleExam14Dec2015Empires.models.units.Swordsman;
+import OOPSampleExam14Dec2015Empires.core.MyReflection;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.StandardLocation;
+import javax.tools.ToolProvider;
+import java.io.File;
 import java.lang.reflect.Constructor;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class UnitFactoryImpl implements UnitFactory {
 
     @Override
     public Unit createUnit(String unitType) {
 
-//        List<Class> classList = null;
-//        try {
-//            classList = getClasses(this.getClass().getClassLoader(),
-//                    "OOPSampleExam14Dec2015Empires.models.units");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        if (classList != null) {
-//            for (Class c : classList) {
-//                if (Objects.equals(c.getSimpleName(), unitType)) {
-//                    try {
-//                        Constructor<Unit> ctor = c.getConstructor();
-//                        Unit unit = (Unit) ctor.newInstance();
-//                        return unit;
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//        }
-//        //throw new UnsupportedOperationException("Unsupported unit type.");
-//        return null;
-
-        switch (unitType) {
-            case "Archer":
-                return new Archer();
-            case "Swordsman":
-                return new Swordsman();
-            default:
-                throw new UnsupportedOperationException("Unsupported unit type.");
+        final String pack = "OOPSampleExam14Dec2015Empires.models.units";
+        Collection<Class> classList = new ArrayList<>();
+        Constructor[] ctors = null;
+        Unit unit = null;
+        try {
+            classList = MyReflection.getClasses(pack);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
+        for(Class c : classList) {
+            if ((c.getSimpleName().toLowerCase()).equals(unitType.toLowerCase())) {
+                ctors = c.getConstructors();
+                if (ctors != null) {
+                    try {
+                        unit = (Unit) ctors[0].newInstance();
+                    } catch (InstantiationException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return unit;
 
-//    private static List<Class> getClasses(ClassLoader cl, String pack) throws Exception {
-//
-//        URL upackage = cl.getResource(pack);
-//
-//        BufferedReader br = null;
-//        Object upackageContent = null;
-//        if (upackage != null) {
-//            upackageContent = upackage.getContent();
+//        switch (unitType) {
+//            case "Archer":
+//                return new Archer();
+//            case "Swordsman":
+//                return new Swordsman();
+//            default:
+//                throw new UnsupportedOperationException("Unsupported unit type.");
 //        }
-//        InputStreamReader isr = null;
-//        if (upackageContent != null) {
-//            isr = new InputStreamReader((InputStream) upackageContent);
-//        }
-//        if (isr != null) {
-//            br = new BufferedReader(isr);
-//        }
-//        String dottedPackage = pack.replaceAll("[/]", ".");
-//
-//        List<Class> classes = new ArrayList<>();
-//        String line = null;
-//        if (br != null) {
-//            line = br.readLine();
-//        }
-//        while (line != null) {
-//            if(line.endsWith(".class")) {
-//                int lastIndex = line.lastIndexOf('.');
-//                if (lastIndex >= 0) {
-//                    String className = dottedPackage + "." + line.substring(0, lastIndex);
-//                    Class c = Class.forName(className);
-//                    classes.add(c);
-//                }
-//            }
-//            line = br.readLine();
-//        }
-//        return classes;
-//    }
+    }
 }
